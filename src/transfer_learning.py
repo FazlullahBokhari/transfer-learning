3430a9d05626fdb5ee23c6063bf8437da52a4866
fazlullahb0 <fazlullahb0@gmail.com>
2022-03-04 13:45:04 +0530

    done

--- src/transfer_learning.py
+++ src/transfer_learning.py
@@ -27,6 +27,25 @@ def main(config_path):
     base_model = tf.keras.models.load_model(base_model_path)
     base_model.summary()
 
+    ## freeze weights
+
+    for layer in base_model.layers[: -1]:
+        print(f"before freezing weights {layer.name}: {layer.trainable}")
+        layer.trainable = False
+        print(f"after freezing weights {layer.name}: {layer.trainable}")
+
+    ## modify last layer for our problem statements
+
+    base_layers = base_model.layers[: -1]
+
+    new_model = tf.keras.models.Sequential(base_layers)
+
+    new_model.add(
+        tf.keras.layers.Dense(2, activation="softmax", name="output_layer")
+    )
+
+    new_model.summary()
+
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
