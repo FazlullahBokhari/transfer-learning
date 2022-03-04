baccdf1a0a0d9b4bbee982266dd0200213d5d1a9
fazlullahb0 <fazlullahb0@gmail.com>
2022-03-04 13:30:16 +0530

    done

---
+++ src/transfer_learning.py
@@ -0,0 +1,43 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+from utils.common import read_yaml, create_directories
+import random
+import tensorflow as tf
+
+
+STAGE = "transferlearning" ## <<< change stage name 
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+def main(config_path):
+    ## read config files
+    config = read_yaml(config_path)
+
+    # load Base model
+    base_model_path = os.path.join("artifacts", "models/base_models.h5")
+    base_model = tf.keras.models.load_model(base_model_path)
+    base_model.summary()
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
